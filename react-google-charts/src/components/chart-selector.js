8ca67b527575984ff1a41465197b035d692b790d
Felicity L <felicette@icloud.com>
2016-07-15 00:36:00 -0500

    created chart selector component

---
+++ react-google-charts/src/components/chart-selector.js
@@ -0,0 +1,40 @@
+import React, { Component } from 'react'
+
+export default class ChartSelector extends Component {
+	constructor(props) {
+		super(props)
+
+		this.state = { selectedChart: props.defaultChart }
+	}
+
+	handleChange = (event) => {
+		this.setState({selectedChart: event.target.value})
+		this.props.handler(event)
+	}
+
+	render() {
+		const _chartTypes = [ "BarChart"
+												, "LineChart"
+												, "Histogram"
+												, "ScatterChart"
+												, "AreaChart"
+												, "ColumnChart"
+												, "ComboChart"
+												, "PieChart"
+												, "SteppedAreaChart" ]
+
+		const _buildOptions = function(chartType) {
+			return (
+				<option key={ `${chartType}` } value={`${chartType}`}>
+					{`${chartType}`}
+				</option> 
+			)
+		}
+
+		return (
+			<select value={ this.state.selectedChart } onChange={ this.handleChange }>
+				{ _chartTypes.map(_buildOptions) }
+			</select>
+		)
+	}
+}
