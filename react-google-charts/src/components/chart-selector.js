b7b4a48f69184ee7bbec35d8e28782c5ffd04d5b
Felicity L <felicette@icloud.com>
2016-07-21 02:09:12 -0500

    fixup! tabs lol

--- react-google-charts/src/components/chart-selector.js
+++ react-google-charts/src/components/chart-selector.js
@@ -3,15 +3,15 @@ import React, { Component } from 'react'
 export default class ChartSelector extends Component {
     constructor(props) {
 	super(props)
-	
+
 	this.state = { selectedChart: props.defaultChart }
     }
-    
+
     handleChange = (event) => {
 	this.setState({selectedChart: event.target.value})
 	this.props.handler(event)
     }
-    
+
     render() {
 	const _chartTypes = [ "BarChart"
 			    , "LineChart"
@@ -22,15 +22,15 @@ export default class ChartSelector extends Component {
 			    , "ComboChart"
 			    , "PieChart"
 			    , "SteppedAreaChart" ]
-	
+
 	const _buildOptions = function(chartType) {
 	    return (
 		<option key={ `${chartType}` } value={`${chartType}`}>
 		    {`${chartType}`}
-		</option> 
+		</option>
 	    )
 	}
-	
+
 	return (
 	    <select value={ this.state.selectedChart } onChange={ this.handleChange }>
 		{ _chartTypes.map(_buildOptions) }
